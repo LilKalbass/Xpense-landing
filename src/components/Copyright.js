5ab35b79b0d25670ce20d1b8811c7652fcb4c891
Artem Dimoglov <adimoglov@gmail.com>
2022-10-08 11:06:14 +0300

    done

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { copyright } from '../data';
+import {copyright} from '../data';
 
 const Copyright = () => {
   return (
-      <div
-          className = "flex flex-col items-center gap-y-2 lg:flex-row lg:justify-between" data-aos = "fade-up" data-aos-delay = "200">
+      <div className = "flex flex-col items-center gap-y-2 lg:flex-row lg:justify-between" data-aos = "fade-up" data-aos-delay = "200">
         <div className= "flex gap-x-6">
-          <a className= "hover:text-accent transition" href={copyright.link1.href}>
-            {copyright.link1.name}
-          </a>
-          <a className= "hover:text-accent transition" href={copyright.link2.href}>
-            {copyright.link2.name}
-          </a>
+          <a className= "hover:text-accent transition" href={copyright.link1.href}>{copyright.link1.name}</a>
+          <a className= "hover:text-accent transition" href={copyright.link2.href}>{copyright.link2.name}</a>
         </div>
         <div>{copyright.copyText}</div>
         <ul className = "flex gap-x-[12px]">
-          {copyright.social.map((item, index) => {
+            {copyright.social.map((item, index) => {
             return (
                 <li key={item.index}>
                   <a href={item.href}>
